1f55096280ae36dd6c4d81a213c0016e97c86375
Gabrielpicirili <gabriel.picirili@aluno.senai.br>
2023-08-18 09:17:12 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,88 @@
+/*const name = ["Whinds", "Freeway",  "Teste", "Maria"];
+
+names.array.forEach((name) => {
+    console.log(`Hi, ${name}!`);
+
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(numbers);
+
+
+numbers.forEach((number, index) => {
+    if(number % 2 == 0){
+    console.log(number + `está na posição` + index  );
+}
+    
+});
+
+const cars = [
+    {
+        marca: "ford",
+        modelo: "Focus",
+    },
+    
+    {
+        marca: "BMW",
+        modelo: "BMW z4",
+    },
+    
+    {
+        marca: "fiat",
+        modelo: "palio",
+    },
+    
+    {
+        marca: "audi",
+        modelo: "A3",
+    },
+    
+];
+cars.forEach((car) => {
+    console.log(`marca: ${car.marca}
+    modelo: ${car.modelo}`);
+});
+
+class Car {
+ constructor(param1, param2){
+    this.brand = param1;
+    this.model = param2;
+ }
+
+}*/
+ class CarsList{
+    constructor(){
+        this.cars = []
+    }
+
+addCar(param) {
+    this.cars.push(param);
+}
+
+
+getnewcar(car){
+    this.cars.push(car);
+}
+}
+
+
+
+
+
+
+function createcar(){
+    const brand = document.getElementById(brandInput).value;
+    const model = document.getElementById(modelInput).value;
+
+
+    const car = new Car(brand, model);
+
+List.addCar(hotweels)
+
+    
+    showhtml()
+}
+
+function showhtml(){
+
+}
